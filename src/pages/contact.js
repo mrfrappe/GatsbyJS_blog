3d4aa4a36dde4e3d1365f0aad299ddb9215027be
Karol Kawski <karol.kawski@protonmail.com>
2020-09-06 15:28:36 +0200

    [update] clear unecessary files

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const ContactPage = () => (
+    <>
+        <h1>Hi from the second page</h1>
+        <p>Welcome to page 2</p>
+    </>
+)
+
+export default ContactPage
